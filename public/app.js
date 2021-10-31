221243a2692c1ab466878cbf2e1e37e3e3f6e88e
diegoarcos7 <arcos.5c@gmail.com>
2021-10-30 22:07:25 -0500

    set value arrays

---
+++ public/app.js
@@ -0,0 +1,87 @@
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+
+const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+]
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2021,10,1,11,30,0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+
+
+giveaway.textContent = `Giveaway Ends On ${weekday}, ${date} ${month} ${year} ${hours}: ${minutes}am`;
+
+//Future time in ms
+const futureTime = futureDate.getTime();
+console.log(futureTime);
+
+function getRemainingTime(){
+    const today = new Date().getTime();
+    const t = futureTime - today;
+    console.log(t);
+    //1s = 1000s
+    //1m = 60s
+    //1hr = 60min
+    //1d = 24hr
+
+    // values in ms
+    const oneDay = 24*60*60*1000;
+    const oneHour = 60*60*1000;
+    const oneMinute = 60*1000;
+    //Calculate all values
+    let days = t/oneDay
+    days = Math.floor(days);
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    let seconds = Math.floor((t % oneMinute) / 1000);
+
+    //Set value arrays
+    const values = [days,hours,minutes,seconds];
+
+    function format(item){
+        if(item < 10){
+            return item = `0${item}`
+        }
+        return item
+    }
+
+    items.forEach(function(item,index){
+        item.innerHTML = format(values[index]);
+    });
+
+    //MINUTO 5:44:53
+}
+
+getRemainingTime();
